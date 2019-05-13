1543772df864b8ffb91d39b5adfda517524c5e79
noronhasandrew <36646800+noronhasandrew@users.noreply.github.com>
2019-05-13 19:54:53 -0300

    Add files via upload

---
+++ src/main/java/com/respondeaqui/RespondeAquiApplication.java
@@ -0,0 +1,13 @@
+package com.respondeaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespondeAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespondeAquiApplication.class, args);
+	}
+
+}
