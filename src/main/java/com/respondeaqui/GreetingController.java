9d0c332cc0c4a247104c74af7d46f6e281a57dcf
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2019-05-18 12:13:54 -0300

    Página de login

--- src/main/java/com/respondeaqui/GreetingController.java
+++ src/main/java/com/respondeaqui/GreetingController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+	@GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+	
+	@GetMapping("/fragments/bannerInitial")
+    public String bannerInitial() {
+        return "bannerInitial";
+    }
+	
 	@GetMapping("/timeline")
     public String timeline() {
         return "timeline";
